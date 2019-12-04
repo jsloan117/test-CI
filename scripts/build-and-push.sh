aeb868b37bc806f3104dc5d9d649a3797b0903e5
Jonathan Sloan <jsloan117@gmail.com>
2019-12-03 19:41:34 -0600

    updating CI files to fix failing pipelines-70

--- scripts/build-and-push.sh
+++ scripts/build-and-push.sh
@@ -12,22 +12,29 @@ fi
 export IMAGE_TAG
 
 build_images () {
+  echo -e "\n<<< Building default image >>>\n"
   docker build -f Dockerfile -t "${IMAGE_NAME}":"${CI_PLATFORM}"-"${IMAGE_TAG}" .
+  echo -e "\n<<< Building ubuntu image >>>\n"
   docker build -f Dockerfile.ubuntu -t $"${IMAGE_NAME}":ubuntu-"${CI_PLATFORM}"-"${IMAGE_TAG}" .
 }
 
 test_images () {
+  echo -e "\n<<< Testing default image >>>\n"
   docker run --rm "${IMAGE_NAME}":"${CI_PLATFORM}"-"${IMAGE_TAG}" bash --version
+  echo -e "\n<<< Testing ubuntu image >>>\n"
   docker run --rm "${IMAGE_NAME}":ubuntu-"${CI_PLATFORM}"-"${IMAGE_TAG}" bash --version
 }
 
 push_images () {
+  echo -e "\n<<< Pushing default image >>>\n"
   docker push "${IMAGE_NAME}":"${CI_PLATFORM}"-"${IMAGE_TAG}"
+  echo -e "\n<<< Pushing ubuntu image >>>\n"
   docker push $"${IMAGE_NAME}":ubuntu-"${CI_PLATFORM}"-"${IMAGE_TAG}"
 }
 
 build_images
 test_images
-if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" -o "${TRAVIS_BRANCH}" = "dev" ]; then
+#if [[ "${TRAVIS_PULL_REQUEST}" = false ]] && [[ "${TRAVIS_BRANCH}" =~ ^(dev|master)$ ]]; then
+if [ "${TRAVIS_PULL_REQUEST}" = false ] && [ "${TRAVIS_BRANCH}" = master -o "${TRAVIS_BRANCH}" = dev ]; then
   push_images
 fi
