f36cb1fa1f0bf24cd5448efe34cd304cad2d2811
Jonathan Sloan <jsloan117@gmail.com>
2019-12-03 20:24:13 -0600

    updating CI files to fix failing pipelines-72

--- scripts/doc-build-and-deploy.sh
+++ scripts/doc-build-and-deploy.sh
@@ -5,11 +5,11 @@ build_documentation () {
   mkdir -p "$HOME/.ssh"
   echo "${DEPLOY_KEY}" | base64 --decode > "$HOME/.ssh/id_rsa"
   chmod 600 "$HOME/.ssh/id_rsa"
-  echo -e '\n<<< Setting remote origin to SSH >>>\n'
+  echo -e '\n<<< Setting remote origin to SSH >>>'
   git remote set-url origin git@github.com:jsloan117/test-CI.git
-  echo -e '\n<<< Setting Git name for commits >>>\n'
+  echo -e '\n<<< Setting Git name for commits >>>'
   git config --global user.name 'TravisCI'
-  echo -e '\n<<< Installing mkdocs & theme >>>\n'
+  echo -e '\n<<< Installing mkdocs & theme >>>'
   pip3 -q install mkdocs mkdocs-material pygments
   echo ''
   mkdocs build -cs
